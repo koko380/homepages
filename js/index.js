72961770334321c0c99327b0c0b37c88d9328aab
koko380 <79810470+koko380@users.noreply.github.com>
2021-03-08 10:43:59 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 //スクロール出現
 let scroll = document.getElementsByClassName("scroll");
-function scroll_show(){
+function scrollShow(){
 	for(let i = 0; i < scroll.length; i++) {
     if (window.innerHeight > scroll[i].getBoundingClientRect().top + 75) {
 	    scroll[i].classList.add('scrollShow');
@@ -10,21 +10,42 @@ function scroll_show(){
 	}
 }
 window.addEventListener("load", event => {
-	scroll_show();
+	scrollShow();
 });
 window.addEventListener("scroll", event => {
-  scroll_show();
+  scrollShow();
 });
 
 //メニュー
 let menu = document.getElementsByClassName("menu")[0];
 let content = document.getElementsByClassName("content")[0];
-menu.addEventListener("click", event => {
+function contentShow(){
 	if(content.classList.contains("contentShow") === false){
-		content.classList.add("contentShow");
-	} else {
-		content.classList.remove("contentShow");
-	}
+    content.classList.add("contentShow");
+  } else {
+    content.classList.remove("contentShow");
+  }
+}
+menu.addEventListener("click", event => {
+	contentShow();
+});
+
+//メニューを押した時のリンク
+let profileMenu = document.getElementById("profileMenu");
+let skillMenu = document.getElementById("skillMenu");
+let educationMenu = document.getElementById("educationMenu");
+let workMenu = document.getElementById("workMenu");
+profileMenu.addEventListener("click", event => {
+	contentShow();
+});
+skillMenu.addEventListener("click", event => {
+  contentShow();
+});
+educationMenu.addEventListener("click", event => {
+  contentShow();
+});
+workMenu.addEventListener("click", event => {
+  contentShow();
 });
 
 window.onload = function(){
